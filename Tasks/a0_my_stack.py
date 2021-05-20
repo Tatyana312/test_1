5e33172abf479c0895c1d4fecbbdbd5e233a3672
Tatyana312 <avrorik5@yandex.ru>
2021-05-20 19:52:42 +0300

    a0_done_list

--- Tasks/a0_my_stack.py
+++ Tasks/a0_my_stack.py
@@ -3,15 +3,17 @@ My little Stack
 """
 from typing import Any
 
-my_stack = []
+my_stack = []  # в качестве стека список, чтобы функции работали как стек  список. Вершина справа. Как из списка достать последний элемент,
+
 
 def push(elem: Any) -> None:
     """
     Operation that add element to stack
-
+добавляет элемент на вершину стека
     :param elem: element to be pushed
     :return: Nothing
     """
+    my_stack.append(elem)
     print(elem)
     return None
 
@@ -19,36 +21,52 @@ def push(elem: Any) -> None:
 def pop() -> Any:
     """
     Pop element from the top of the stack. If not elements - should return None.
-
+  #принимает индекс должны вернуть элемент  с вершины - элемент. Из стека не удаляется. Из стека не удаляет
+  достать элемент последнйи и удалить его
     :return: popped element
     """
-    return None
+    last_elem = my_stack.pop() #заьирает элемент с конца и удаляет его
+
+    return last_elem
 
 
 def peek(ind: int = 0) -> Any:
     """
     Allow you to see at the element in the stack without popping it.
-
+    увидеть элемент без его снятия
     :param ind: index of element (count from the top, 0 - top, 1 - first from top, etc.)
     :return: peeked element or None if no element in this place
     """
+
+    print(my_stack[-ind])
     print(ind)
-    return None
+    return my_stack[-1-ind]
 
 
 def clear() -> None:
     """
     Clear my stack
-
+    должно чистить функцию
     :return: None
     """
+
+    my_stack.clear()
+    print(my_stack)
     return None
 
 if __name__ == '__main__':
     push(1)
     print(my_stack)
     push(100)
+    push(1000)
     print(my_stack)
+    pop()
+    peek(0)
+    clear()
+
+
+
+
     # новое задание от Master
     # перед checkout обязательно commit cntrl+k
 
